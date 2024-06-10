d8bb9283731ac2060de8f44d4774ca5f6e3dda83
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-10 10:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let p=document.getElementById('status');
+function enter(params) {
+	let h=document.createElement('h1');
+	h.innerHTML='Entered Metaverse';
+	p.innerHTML='';
+	p.appendChild(h);
+}
